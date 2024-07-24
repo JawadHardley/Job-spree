b39384bc1bfa4916b00ae299120fab34962ab424
JawadHardley <jawadcharls@gmail.com>
2024-07-25 00:02:07 +0300

    Working with the view

--- pixel-positions/resources/js/app.js
+++ pixel-positions/resources/js/app.js
@@ -1,1 +1,8 @@
-import './bootstrap';
+import "./bootstrap";
+// Import our custom CSS
+import "../css/app.css";
+
+// Import all of Bootstrap's JS
+import * as bootstrap from "bootstrap";
+
+import.meta.glob(["../images/**"]);
---
+++ pixel-positions/resources/views/components/layout.blade.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap w/ Vite</title>
+    <script type="module" src="./js/main.js"></script>
+    @vite(['resources/js/app.js', 'resources/css/app.css'])
+  </head>
+  <body class="bg-dark">
+    <nav class="navbar navbar-expand-lg bg-dark border-bottom border-body" data-bs-theme="dark">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">
+                <img src="{{ Vite::asset('resources/images/logo.svg') }}" alt="">
+              </a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav mx-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Jobs</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Careers</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Salaries</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Companies</a>
+                  </li>
+                </ul>
+              </div>
+              <a href="#" class="text-decoration-none text-white">
+                <i class="bi bi-check-square-fill text-primary"></i>
+                Post a job
+              </a>
+            </div>
+      </nav>
+  </body>
+</html>
