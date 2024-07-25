43b6f30f9405db94e6d448585d059692e0e36565
JawadHardley <jawadcharls@gmail.com>
2024-07-25 23:40:56 +0300

    the end round 4

--- pixel-positions/resources/views/components/jobCardWide.blade.php
+++ pixel-positions/resources/views/components/jobCardWide.blade.php
@@ -1,16 +1,16 @@
-<div class="col-12 my-3 p-4 rounded bg-dark">
+<div class="col-12 my-3 p-3 rounded bg-dark">
     <a href="#1" class="text-decoration-none">
         <div class="row">
             <div class="col-2">
-                <img src="http://placehold.it/100/100" class="rounded img-fluid" alt="">
+                <img src="http://placehold.it/120/120" class="rounded img-fluid" alt="">
             </div>
             <div class="col-8">
-                <h6 class="lead fs-6">GovGen</h6>
-                <h2 class="fs-4">Full Stack Developer</h2>
+                <h6 class="lead fs-6 text-secondary">GovGen</h6>
+                <h2 class="fs-4 text-white">Full Stack Developer</h2>
             </div>
             <div class="col-2">
-                <span class="btn lead btn-sm rounded-pill btn-outline-secondary">Remote</span>
-                <span class="btn lead btn-sm rounded-pill btn-outline-secondary">22h</span>
+                <span class="btn mx-1 float-end lead btn-sm rounded-pill btn-outline-secondary">Remote</span>
+                <span class="btn mx-1 float-end lead btn-sm rounded-pill btn-outline-secondary">22h</span>
             </div>
         </div>
         <div class="row">
@@ -18,9 +18,11 @@
 
             </div>
             <div class="col-10">
-                <h6 class="lead fs-6">Full time from - $60,000</h6>
-                <span class="float-end"><x-tag href="#2">API</x-tag></span>
+                <h6 class="lead fs-6 text-secondary">Full time from - $60,000</h6>
             </div>
         </div>
+            <x-tag href="#2" class="float-end mx-1">API</x-tag>
+            <x-tag href="#2" class="float-end mx-1">Backend</x-tag>
+            <x-tag href="#2" class="float-end mx-1">NodeJs</x-tag>
     </a>
 </div>
--- pixel-positions/resources/views/components/tag.blade.php
+++ pixel-positions/resources/views/components/tag.blade.php
@@ -1,1 +1,2 @@
-<a {{ $attributes }} class="px-2 py-1 text-decoration-none rounded-pill text-bg-secondary" style="font-size: 10px">{{ $slot }}</a>
+<a {{ $attributes->merge(['class' => "px-2 py-1 text-decoration-none rounded-pill text-bg-secondary"]) }} style="font-size: 10px">{{ $slot }}</a>
+
