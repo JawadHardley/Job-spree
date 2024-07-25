a6522e5066bc94214e69d69ef70e8c8fa2631fe8
JawadHardley <jawadcharls@gmail.com>
2024-07-25 23:29:20 +0300

    the end round 3

--- pixel-positions/resources/views/components/jobCardWide.blade.php
+++ pixel-positions/resources/views/components/jobCardWide.blade.php
@@ -1,33 +1,25 @@
-<div class="col-12 my-3">
+<div class="col-12 my-3 p-4 rounded bg-dark">
     <a href="#1" class="text-decoration-none">
-        <div class="card bg-dark text-white" style="">
-            <div class="card-body">
-              <div class="row">
-                <div class="col-2">
-                  <div class="col-2 me-auto px-2">
-                    <img src="http://placehold.it/100/100" class="rounded" alt="">
-                  </div>
-                </div>
-                <div class="col-8">
-                    <span class="card-title">GovGen</span>
-                    <h6 class="card-subtitle mb-2 fs-3 py-3">Software Developer</h6>
-                </div>
-                <div class="col-2">
-                </div>
-              </div>
-              <div class="row">
-                <div class="col-2 bg-danger">x</div>
-                <div class="col-8 bg-primary">
-                    <p class="card-text fs-6">Full time from - $60,000</p>
-                </div>
-                <div class="col-2 bg-warning">
-                    <x-tag>API</x-tag>
-                    <x-tag>Backend</x-tag>
-                    <x-tag>NodeJs</x-tag>
-                </div>
-              </div>
+        <div class="row">
+            <div class="col-2">
+                <img src="http://placehold.it/100/100" class="rounded img-fluid" alt="">
+            </div>
+            <div class="col-8">
+                <h6 class="lead fs-6">GovGen</h6>
+                <h2 class="fs-4">Full Stack Developer</h2>
+            </div>
+            <div class="col-2">
+                <span class="btn lead btn-sm rounded-pill btn-outline-secondary">Remote</span>
+                <span class="btn lead btn-sm rounded-pill btn-outline-secondary">22h</span>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-2">
 
-
+            </div>
+            <div class="col-10">
+                <h6 class="lead fs-6">Full time from - $60,000</h6>
+                <span class="float-end"><x-tag href="#2">API</x-tag></span>
             </div>
         </div>
     </a>
--- pixel-positions/resources/views/components/tag.blade.php
+++ pixel-positions/resources/views/components/tag.blade.php
@@ -1,3 +1,1 @@
-<a {{ $attributes }}>
-    <span class="badge rounded-pill text-bg-secondary">{{ $slot }}</span>
-</a>
+<a {{ $attributes }} class="px-2 py-1 text-decoration-none rounded-pill text-bg-secondary" style="font-size: 10px">{{ $slot }}</a>
