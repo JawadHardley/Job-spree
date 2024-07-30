b3c9a631ebfe071c50553f8227b962ae4272efa8
JawadHardley <jawadcharls@gmail.com>
2024-07-30 19:29:51 +0300

    minor changes

--- pixel-positions/resources/views/components/layout.blade.php
+++ pixel-positions/resources/views/components/layout.blade.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap w/ Vite</title>
+    <title>pixels</title>
     <script type="module" src="./js/main.js"></script>
     @vite(['resources/js/app.js', 'resources/css/app.css'])
   </head>
