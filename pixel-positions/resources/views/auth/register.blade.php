928bc649adac5a7c234a1960e493a64f99030648
JawadHardley <jawadcharls@gmail.com>
2024-08-01 23:20:17 +0300

    Final 2

---
+++ pixel-positions/resources/views/auth/register.blade.php
@@ -0,0 +1,16 @@
+<x-layout>
+
+    <h1 class="text-white text-center">Register</h1>
+
+    <div class="container text-white">
+        <x-forms.form method="POST" action="/register">
+            <x-forms.input  label="Your Name" name="name" />
+            <x-forms.input  label="email" name="email" type="email" />
+            <x-forms.input  label="password" name="password" type="password" />
+            <x-forms.input  label="password confirmation" name="name" type="password" />
+            <x-forms.input  label="Your Name" name="name" />
+        </x-forms.form>
+    </div>
+
+
+</x-layout>
--- pixel-positions/resources/views/jobs/index.blade.php
+++ pixel-positions/resources/views/jobs/index.blade.php
@@ -1,51 +1,52 @@
 <x-layout>
+        <x-search/>
 
-    <div class="container">
-        <div class="row">
-            <a href="#" class="text-decoration-none text-white">
-                <i class="bi bi-check-square-fill text-primary"></i>
-                Featured Jobs
-              </a>
-        </div>
-        <div class="row">
-            @foreach ($featuredJobs as $job)
-                <x-jobCards :$job />
-            @endforeach
-                {{-- <x-jobCards></x-jobCards>
-                <x-jobCards></x-jobCards> --}}
-        </div>
-    </div>
-
-    <div class="container">
-        <div class="row mt-4">
-            <a href="#" class="text-decoration-none text-white">
-                <i class="bi bi-check-square-fill text-primary"></i>
-                Tags
-              </a>
-        </div>
-        <div class="row">
-            <div class="col my-4">
-                @foreach ($tags as $tag)
-                    <x-tag :$tag href="/tag/{{ $tag->name }}">{{ $tag->name }}</x-tag>
+        <div class="container">
+            <div class="row">
+                <a href="#" class="text-decoration-none text-white">
+                    <i class="bi bi-check-square-fill text-primary"></i>
+                    Featured Jobs
+                  </a>
+            </div>
+            <div class="row">
+                @foreach ($featuredJobs as $job)
+                    <x-jobCards :$job />
                 @endforeach
+                    {{-- <x-jobCards></x-jobCards>
+                    <x-jobCards></x-jobCards> --}}
             </div>
         </div>
-    </div>
 
-    <div class="container">
-        <div class="row mt-4">
-            <a href="#" class="text-decoration-none text-white">
-                <i class="bi bi-check-square-fill text-primary"></i>
-                Recent Jobs
-              </a>
+        <div class="container">
+            <div class="row mt-4">
+                <a href="#" class="text-decoration-none text-white">
+                    <i class="bi bi-check-square-fill text-primary"></i>
+                    Tags
+                  </a>
+            </div>
+            <div class="row">
+                <div class="col my-4">
+                    @foreach ($tags as $tag)
+                        <x-tag :$tag href="/tag/{{ $tag->name }}">{{ $tag->name }}</x-tag>
+                    @endforeach
+                </div>
+            </div>
         </div>
-        <div class="row">
-            @foreach ($jobs as $job)
-                <x-jobCardWide :$job />
-            @endforeach
-            {{-- <x-jobCardWide></x-jobCardWide>
-            <x-jobCardWide></x-jobCardWide> --}}
+
+        <div class="container">
+            <div class="row mt-4">
+                <a href="#" class="text-decoration-none text-white">
+                    <i class="bi bi-check-square-fill text-primary"></i>
+                    Recent Jobs
+                  </a>
+            </div>
+            <div class="row">
+                @foreach ($jobs as $job)
+                    <x-jobCardWide :$job />
+                @endforeach
+                {{-- <x-jobCardWide></x-jobCardWide>
+                <x-jobCardWide></x-jobCardWide> --}}
+            </div>
         </div>
-    </div>
 
 </x-layout>
