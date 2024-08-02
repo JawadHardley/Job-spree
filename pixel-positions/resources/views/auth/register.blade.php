4a583f35d5fb9ec50f8fd60b17eb326dfbe6794e
JawadHardley <jawadcharls@gmail.com>
2024-08-02 18:49:46 +0300

    Final 4

--- pixel-positions/resources/views/auth/register.blade.php
+++ pixel-positions/resources/views/auth/register.blade.php
@@ -3,12 +3,11 @@
     <h1 class="text-white text-center">Register</h1>
 
     <div class="container text-white">
-        <x-forms.form method="POST" action="/register" class="mx-auto text-center">
-            <x-forms.input  label="Your Name" name="name" />
-            <x-forms.input  label="email" name="email" type="email" />
-            <x-forms.input  label="password" name="password" type="password" />
-            <x-forms.input  label="password confirmation" name="name" type="password" />
-            <x-forms.input  label="Your Name" name="name" />
+        <x-forms.form method="POST" action="/register" class="mx-auto w-50">
+            <x-forms.input class=""  label="Your Name" name="name" />
+            <x-forms.input class=""  label="email" name="email" type="email" />
+            <x-forms.input class=""  label="password" name="password" type="password" />
+            <x-forms.input class=""  label="password confirmation" name="name" type="password" />
 
         <x-forms.button >Create Account</x-forms.form>
         </x-forms.form>
--- pixel-positions/resources/views/components/forms/button.blade.php
+++ pixel-positions/resources/views/components/forms/button.blade.php
@@ -1,1 +1,1 @@
-<button {{ $attributes(['class' => 'bg-blue-800 rounded py-2 px-6 font-bold']) }}>{{ $slot }}</button>
+<button {{ $attributes(['class' => 'btn btn-primary rounded py-2 px-6']) }}>{{ $slot }}</button>
