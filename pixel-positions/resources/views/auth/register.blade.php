edb42cb1fea4441b59b974afd3a79bd474e6d3df
JawadHardley <jawadcharls@gmail.com>
2024-08-02 06:33:05 +0300

    final 3

--- pixel-positions/resources/views/auth/register.blade.php
+++ pixel-positions/resources/views/auth/register.blade.php
@@ -3,12 +3,14 @@
     <h1 class="text-white text-center">Register</h1>
 
     <div class="container text-white">
-        <x-forms.form method="POST" action="/register">
+        <x-forms.form method="POST" action="/register" class="mx-auto text-center">
             <x-forms.input  label="Your Name" name="name" />
             <x-forms.input  label="email" name="email" type="email" />
             <x-forms.input  label="password" name="password" type="password" />
             <x-forms.input  label="password confirmation" name="name" type="password" />
             <x-forms.input  label="Your Name" name="name" />
+
+        <x-forms.button >Create Account</x-forms.form>
         </x-forms.form>
     </div>
 
